7d27c8dc409b759fe16d470ec3af9088edef91d9
wenqiangde <andmaster@163.com>
2026-06-30 18:00:54 +0800

    agentsetup v1.1.1

--- Formula/agentsetup.rb
+++ Formula/agentsetup.rb
@@ -1,26 +1,26 @@
 class Agentsetup < Formula
   desc "Versioned distribution and setup tool for agent rules and skills"
   homepage "https://github.com/wenqiangde/agentsetup"
-  version "1.1.0"
+  version "1.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.0/agentsetup-v1.1.0-darwin-arm64"
-      sha256 "2cbfae7417e7bb68f00c174376ae7953cd7393cbabce80dde95acb6356018159"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.1/agentsetup-v1.1.1-darwin-arm64"
+      sha256 "6530ff5bfb0e3983cc94c93889fabcd9e10cd92d392fc9f133800cba1e1c95c4"
     else
-      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.0/agentsetup-v1.1.0-darwin-amd64"
-      sha256 "6dd1fa9a7cd2e6f6cbcbb5786ca9047cac5a4dbf54418702b4b11e2cf4dde3ff"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.1/agentsetup-v1.1.1-darwin-amd64"
+      sha256 "e07ef5c282298daa47af8bcf6e880326b520d1fd8e819d02d4ed4689db96b2db"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.0/agentsetup-v1.1.0-linux-arm64"
-      sha256 "9d47b363aa6522c5eebfb0c5a06221132c3d53228bd76721addb4ee61d6305e3"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.1/agentsetup-v1.1.1-linux-arm64"
+      sha256 "14ef7a34aae3dd1dada0efb021ea4cea1456ec94968578370d4366b696fd3184"
     else
-      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.0/agentsetup-v1.1.0-linux-amd64"
-      sha256 "5c937797dc79587c49e551e5e9ee22640bef4b44bbbef33026777186142aae4d"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.1/agentsetup-v1.1.1-linux-amd64"
+      sha256 "b95f91084b3e234840aaa98d115a9f11b278e5e16358f395f2713f0d80313d0c"
     end
   end
 
