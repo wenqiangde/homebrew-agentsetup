00d247ffdbbeda65ca1805eb782529da13fc2b51
wenqiangde <andmaster@163.com>
2026-06-30 18:43:12 +0800

    agentsetup v1.1.2

--- Formula/agentsetup.rb
+++ Formula/agentsetup.rb
@@ -1,26 +1,26 @@
 class Agentsetup < Formula
   desc "Versioned distribution and setup tool for agent rules and skills"
   homepage "https://github.com/wenqiangde/agentsetup"
-  version "1.1.1"
+  version "1.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.1/agentsetup-v1.1.1-darwin-arm64"
-      sha256 "75a401455e26254e51d775db28aaee5cec5e73e24600dd6bedc77a78e12d675d"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.2/agentsetup-v1.1.2-darwin-arm64"
+      sha256 "5f458104121e0e5ff7756969577dd2d4367f8755755be56226925b5b451f26c1"
     else
-      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.1/agentsetup-v1.1.1-darwin-amd64"
-      sha256 "47e7110362f22141e699345e9153e45570f7dbd04904c2583e4ca2e91b49dcf2"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.2/agentsetup-v1.1.2-darwin-amd64"
+      sha256 "8a040c711d3e608a334320952c278acd4fe0d93344adc58246dca841623e4842"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.1/agentsetup-v1.1.1-linux-arm64"
-      sha256 "c60389e94315dfd5400e9e9be364b73c8407b49d8a19a261d3841c175ed475c8"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.2/agentsetup-v1.1.2-linux-arm64"
+      sha256 "d8fe7ea98c902c04c14316b93c9634f2242331b419ccca7afd54834b2acb8eb1"
     else
-      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.1/agentsetup-v1.1.1-linux-amd64"
-      sha256 "adf74b9f4d75c6cba175c0dee4ad9911d021b7ab9833d51a94e4c3285277d469"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.1.2/agentsetup-v1.1.2-linux-amd64"
+      sha256 "b5d45997928a494a848bec30b460cda885728e2b7514c43c35ace14a32797a9d"
     end
   end
 
