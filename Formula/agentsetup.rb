75e6f8f71be789e9c5a6c4fe6684e5d93605381b
Wenqiang <andmaster@163.com>
2026-08-02 11:17:20 +0800

    agentsetup 1.3.6

--- Formula/agentsetup.rb
+++ Formula/agentsetup.rb
@@ -1,26 +1,26 @@
 class Agentsetup < Formula
   desc "Versioned distribution and setup tool for agent rules and skills"
   homepage "https://github.com/wenqiangde/agentsetup"
-  version "1.3.5"
+  version "1.3.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.5/agentsetup-v1.3.5-darwin-arm64"
-      sha256 "c9b7773c84157dd2bc527ab5cda20540ae27832ba3d7ce98ea4baa3c3e97d3b6"
+      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.6/agentsetup-v1.3.6-darwin-arm64"
+      sha256 "0443b8306d540a70959633db21b94f18f1ba5310f38392c0048b5de3fa7f08cf"
     else
-      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.5/agentsetup-v1.3.5-darwin-amd64"
-      sha256 "2a7aa0bbb39ea37ed8bfc7302dbb46f6bcb307f2665dfb351b5598775b6d15e4"
+      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.6/agentsetup-v1.3.6-darwin-amd64"
+      sha256 "941a0d32015928f0e56ed02d06823cd383d6366300e65b96b2859cd0d5cbc155"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.5/agentsetup-v1.3.5-linux-arm64"
-      sha256 "9b5d7fb92e5aa41c8d1af38f240773d15701a18f731ca142a5e812f9e2de476f"
+      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.6/agentsetup-v1.3.6-linux-arm64"
+      sha256 "c64193c057456e28ceb5225546fd992ae5cad75ca8592a0d634d6e78a22b49ff"
     else
-      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.5/agentsetup-v1.3.5-linux-amd64"
-      sha256 "1097f95fa8d6c13711956fdcdcc6232798aab87d80236bb03a54517a90305dc3"
+      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.6/agentsetup-v1.3.6-linux-amd64"
+      sha256 "9871a80465826d8c37bbbd3b9132ff73915e95a4873d9aba0348b6b09bf77a60"
     end
   end
 
