9518df06c317746f95575ec4db67c22449d888df
wenqiangde <andmaster@163.com>
2026-08-03 17:01:17 +0800

    chore: update agentsetup v1.3.8

--- Formula/agentsetup.rb
+++ Formula/agentsetup.rb
@@ -1,26 +1,26 @@
 class Agentsetup < Formula
   desc "Versioned distribution and setup tool for agent rules and skills"
   homepage "https://github.com/wenqiangde/agentsetup"
-  version "1.3.6"
+  version "1.3.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.6/agentsetup-v1.3.6-darwin-arm64"
-      sha256 "19c3f06ffcdc1aeb466dfa6cae4dc59556dc54c2c24b3149959b09db743dce45"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.3.8/agentsetup-v1.3.8-darwin-arm64"
+      sha256 "4d6df8dccf3a37b6a5e136dc2af0ae4f4a59023404d807eef03ea611205ea8f8"
     else
-      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.6/agentsetup-v1.3.6-darwin-amd64"
-      sha256 "ad79bf0a51ffd0d2b247575dc7d33570d6a58554eaa53b2b55d247c7ef523790"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.3.8/agentsetup-v1.3.8-darwin-amd64"
+      sha256 "219b28c08288bd84711c6016f37023a0ae531305cd193b0af24a16e123b49637"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.6/agentsetup-v1.3.6-linux-arm64"
-      sha256 "86ba10ee965a7a3b9e49ecb2971d7d39d2724d0f106a1161a0a60a9a187f2a82"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.3.8/agentsetup-v1.3.8-linux-arm64"
+      sha256 "a21fb57011bc761ecba6bec58053a31d0f820fcc688874ccadb3ac690f241666"
     else
-      url "https://github.com/wenqiangde/homebrew-agentsetup/releases/download/v1.3.6/agentsetup-v1.3.6-linux-amd64"
-      sha256 "d0d3ffe4689b76e17759f309b0dcc663951aad25e927e2c51f8538df56e579fd"
+      url "https://github.com/wenqiangde/agentsetup/releases/download/v1.3.8/agentsetup-v1.3.8-linux-amd64"
+      sha256 "010f9b1b55796e17103e412667b64a9be7c517999149b9505dc122441f7773e0"
     end
   end
 
